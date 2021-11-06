a98c998702963e8782f0427dea7272bbdd2dea36
ryanterronez <ryanterronez@gmail.com>
2021-11-06 13:23:14 -0500

    Wrote test3 to see if 21 character string passed, it passed

--- tests.py
+++ tests.py
@@ -3,13 +3,20 @@ from check_pwd import check_pwd
 
 
 class TestSuite(unittest.TestCase):
+    #  empty string, should fail
     def test1(self):
         password = ""
         self.assertFalse(check_pwd(password))
 
+    # 8 character string, should pass
     def test2(self):
         password = "12345678"
         self.assertTrue(check_pwd(password))
+
+    # 21 character string, should fail
+    def test3(self):
+        password = "123451234512345123451"
+        self.assertFalse(check_pwd(password))
 
 
 if __name__ == '__main__':
