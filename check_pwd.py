8ef510a7b514e83e67affddfb79fd0960dcd6876
ryanterronez <ryanterronez@gmail.com>
2021-11-06 14:14:32 -0500

    fixed issue with checking for invalid characters

--- check_pwd.py
+++ check_pwd.py
@@ -8,10 +8,10 @@ def check_pwd(password):
     password_non_approved_characters = [c for c in password if c in password_lowercase_list and
                                         c in password_uppercase_list and c in password_integer_list
                                         and c in password_approved_symbols]
-    password_non_approved_length = len(password_non_approved_characters)
     if password == "" or password_length < 8 or password_length > 20\
             or not password_lowercase_list or not password_uppercase_list\
-            or not password_integer_list or password_length != password_non_approved_length:
+            or not password_integer_list or not password_approved_symbols\
+            or password_non_approved_characters:
         return False
     else:
         return True
