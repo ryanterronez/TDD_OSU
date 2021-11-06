d4527d4d13691c71a5f1ebce1001d897540d4869
ryanterronez <ryanterronez@gmail.com>
2021-11-06 13:54:01 -0500

    made function return false if password has no lowercase characters

--- check_pwd.py
+++ check_pwd.py
@@ -1,6 +1,8 @@
 def check_pwd(password):
     password_length = len(password)
-    if password == "" or password_length < 8 or password_length > 20:
+    password_lowercase_list = [c for c in password if c.islower()]
+    if password == "" or password_length < 8 or password_length > 20\
+            or not password_lowercase_list:
         return False
     else:
         return True
