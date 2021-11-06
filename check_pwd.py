0a342b47a01ca9b4b6f762c85cc1c2defc0eeca8
ryanterronez <ryanterronez@gmail.com>
2021-11-06 14:00:36 -0500

    made function return false if password has no digits

--- check_pwd.py
+++ check_pwd.py
@@ -2,8 +2,10 @@ def check_pwd(password):
     password_length = len(password)
     password_lowercase_list = [c for c in password if c.islower()]
     password_uppercase_list = [c for c in password if c.isupper()]
+    password_integer_list = [c for c in password if c.isdigit()]
     if password == "" or password_length < 8 or password_length > 20\
-            or not password_lowercase_list or not password_uppercase_list:
+            or not password_lowercase_list or not password_uppercase_list\
+            or not password_integer_list:
         return False
     else:
         return True
